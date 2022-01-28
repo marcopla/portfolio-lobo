946b1035826e140c29d65bd6fa9557240aa09625
Marco Pla <marco.pla@gmail.com>
2022-01-27 21:10:00 -0300

    adding template string

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ let name = 'texte';
 liElements = document.querySelectorAll('li');
 
 liElements.forEach((element) => {
-  element.innerText += 1;
+  element.innerText += ` 1`;
   console.log(element);
 });
